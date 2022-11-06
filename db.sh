e6dd742375a49074a6b815b5b66154f50a46cfee
Ayobami Lawal <bamiayo90@gmail.com>
2022-11-06 22:51:54 +0100

    changing

--- db.sh
+++ db.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-
-echo "updating apt "
 apt update -y
-
-echo "installing postgres"
 apt install postgresql -y
 
 sudo -i -u postgres bash << EOF
-echo "In"
-echo "creating user"
-echo "creating database altschooldb"
+
 psql
 CREATE USER altschool WITH PASSWORD 'password';
 CREATE DATABASE altschooldb WITH OWNER altschool;
